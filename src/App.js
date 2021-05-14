7589f4ba38a58354f0dec289049510cdd2e2ae18
dungpt <dungphamdev@gmail.com>
2021-05-14 21:44:06 +0700

    reset to default

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-import Header from './components/header'
-
 function App() {
   return (
     <div className="App">
-        <Header/>
+       <h1>hello world</h1>
     </div>
   );
 }
